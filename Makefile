767dbb527b35a1eeab3c35887506eb51a73dc0e7
jngermon <jn.germon@gmail.com>
2015-08-20 23:49:55 +0200

    Add installation of Docker

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 INSTALL_GIT=assets/install-git.sh
+INSTALL_DOCKER=assets/install-docker.sh
 
-all: install-git
+all: install-git \
+	install-docker
 
 install-git:
 	$(INSTALL_GIT)
+
+install-docker:
+	$(INSTALL_DOCKER)
---
+++ assets/install-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Installation of docker"
+
+if [ $(which docker) ]; then
+	echo "Docker is already installed"
+	exit
+fi
+
+if ! [ $(which curl) ]; then
+	echo "Curl is not install"
+	sudo apt-get update
+	sudo apt-get install curl
+fi
+
+curl -sSL https://get.docker.com/ | sh

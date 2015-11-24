350895fca45e7c90addf63b74f273a9634741687
jngermon <jn.germon@gmail.com>
2015-11-24 21:37:51 +0100

    Set installation mode for docker >= 1.7.1

--- assets/install-docker.sh
+++ assets/install-docker.sh
@@ -7,12 +7,18 @@ if [ $(which docker) ]; then
 	exit
 fi
 
-if ! [ $(which curl) ]; then
-	echo "Curl is not install"
-	sudo apt-get update
-	sudo apt-get install curl
-fi
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-curl -sSL https://get.docker.com/ | sh
+sudo cp assets/docker.list /etc/apt/sources.list.d/docker.list
+
+sudo vim /etc/apt/sources.list.d/docker.list
+
+sudo apt-get update
+
+sudo apt-get purge lxc-docker*
+
+sudo apt-get install linux-image-extra-$(uname -r)
+
+sudo apt-get install docker-engine
 
 sudo systemctl enable docker

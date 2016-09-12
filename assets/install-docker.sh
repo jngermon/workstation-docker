e0f484968e8cccf81f67cfbf5c1a2052e55a4bb9
jngermon <jn.germon@gmail.com>
2016-09-12 22:00:49 +0200

    Use official docker install script

--- assets/install-docker.sh
+++ assets/install-docker.sh
@@ -7,21 +7,10 @@ if [ $(which docker) ]; then
 	exit
 fi
 
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-sudo cp assets/docker.list /etc/apt/sources.list.d/docker.list
-
-sudo vim /etc/apt/sources.list.d/docker.list
-
-sudo apt-get update
-
-sudo apt-get install apt-transport-https ca-certificates
-
-sudo apt-get purge lxc-docker
-
-sudo apt-get install linux-image-extra-$(uname -r)
-
-sudo apt-get install docker-engine
+curl https://get.docker.com > install-docker.sh
+chmod 755 install-docker.sh
+./install-docker.sh
+rm install-docker.sh
 
 sudo systemctl enable docker
 
